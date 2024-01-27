d509df1f0aef11e53795cf9197bca6bb658ab605
auridebson <auridebson@vutus.com>
2024-01-27 18:43:27 -0300

    Update

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
 from flask import Flask
 
 app = Flask(__name__)
+@app.route("/")
+def index():
+    return """
+    <html>
+    <head>
+    </head>
+    <body>
+    
+    <marquee><h1>Hello World</h1></marquee>
+
+    </body>
+    </html>
+    """
+
+
+app.run(debug=True)
